1. correct the GF * pressure matrix multiply index 2. Write -Z to txt 3. correct the sign for np.cross depending on left/right handed 4. Reverse both fault and tetra Z sign in GF function

TetrahedronCoupling/Generate_H5_and_VTU.py
@@ -25,7 +25,8 @@ def transfer_vertices_to_faces(tetra_coords):
 
     # Signed volume function
     def signed_tet_volume(a, b, c, d):
-        return np.dot(np.cross(b - a, c - a), d - a) / 6.0
+        sign = -1 # +1 for right-handed, -1 for left-handed
+        return np.dot(sign * np.cross(b - a, c - a), d - a) / 6.0 
 
     # Allocate result
     N = tetra_coords.shape[0]