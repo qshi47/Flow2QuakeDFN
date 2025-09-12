d65196ffb9b46cf3b59b2f3d0ad9a7fabda28702
Amboy <qshi47@outlook.com>
2025-09-11 23:53:35 -0700

    1. for bunny: use left-handed for txt as well. add progress bar 2. change pore pressure change from -30 MPa to -1 MPa

---
+++ TetrahedronCoupling/StanfordBunny/Generate_PorePressure.jl
@@ -0,0 +1,50 @@
+using DelimitedFiles
+
+
+function Save_time_pressure_to_txt(filename::String, time_array::AbstractVector, pressure::AbstractMatrix)
+    output_matrix = hcat(time_array, pressure)
+    blocksCount = size(pressure, 2)
+
+    # Construct header
+    header = ["time(sec)"]
+    for i in 1:blocksCount
+        push!(header, "cuboid$(i)")
+    end
+
+    # Open file and write header + data
+    open(filename, "w") do io
+        write(io, join(header, ", "))           # write header line
+        write(io, "\n")                        # new line after header
+        writedlm(io, output_matrix, ", ")          # write data rows
+    end
+end
+
+function main(InputTetrahedronFile, OutputPorepressureFile)
+    # Load Reservoir Tetrahedrons
+    data = readdlm(InputTetrahedronFile, ',', skipstart=1)
+    Tetrahedrons_Count = Int(size(data, 1) / 4)
+    println("---- Tetrahedrons count is:  ", Tetrahedrons_Count, " ----")
+
+    # Time for External Stresses
+    Time_Count =  2
+    ExternalStress_TimeArray = zeros(Time_Count)
+    ExternalStress_TimeArray[1]  = 0
+    ExternalStress_TimeArray[2] = 10*365*24*3600 # 10 years in seconds
+
+    # Pore Pressure Change
+    PorePressureChange = zeros(Time_Count, Tetrahedrons_Count)
+    PorePressureChange[1,:] .= 0.0
+    PorePressureChange[2,:] .= -1e6 # -1 MPa
+
+    # Save Pore Pressure Change to a txt file
+    Save_time_pressure_to_txt(OutputPorepressureFile, ExternalStress_TimeArray, PorePressureChange)
+
+    println("---- TXT OutputFile Saved: ", OutputPorepressureFile, " ----")
+end
+
+
+InputTetrahedronFile = "TetrahedronCoupling/Input_Tetrahedron.txt"
+OutputPorepressureFile = "TetrahedronCoupling/Input_PorePressure.txt"
+
+main(InputTetrahedronFile, OutputPorepressureFile)
+
--- TetrahedronCoupling/StanfordBunny/Generate_Tetrahedron.py
+++ TetrahedronCoupling/StanfordBunny/Generate_Tetrahedron.py
@@ -1,6 +1,6 @@
 import gmsh, math, os, tempfile
 import numpy as np
-
+from tqdm import tqdm
 
 def get_vertices(gmsh):
     # Get all node coordinates
@@ -25,15 +25,16 @@ def save_vertices_to_txt(tetra_coords, filename):
     tetra_coords: (N, 4, 3) array: coords of the 4 vertices of N tetrahedrons
     """
     N = tetra_coords.shape[0]
+    tetra_coords[:, :, 2] *= -1 # Flip Z axis to Z up (right-handed)for consistency with Quake-DFN
+    
     with open(filename, "w") as f:
         f.write(f"Index, X, Y, Z \n")
-        for i in range(N):
+        for i in tqdm( range(N), desc="tetrahedrons"):
             for j in range(4):
                 f.write(f"{i+1}, {tetra_coords[i, j, 0]}, {tetra_coords[i, j, 1]}, {tetra_coords[i, j, 2]}\n")
 
-
 stl = "./TetrahedronCoupling/StanfordBunny/Stanford_Bunny.stl" 
-lc  = 100.0  # target element size
+
 
 gmsh.initialize()
 gmsh.model.add("bunny")
